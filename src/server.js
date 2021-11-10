0d638c1e87f3fb55c389bedd6d4542581d80280e
Hyejin Eom <yazbyz@gmail.com>
2021-11-11 00:21:26 +0900

    [ADD] WebSocket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -11,4 +13,9 @@ app.get("/", (req, res) => res.render("home")); // render 세팅
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app); // access to http server
+const wss = new WebSocket.Server({ server }); // http + ws server on same port
+
+server.listen(3000, handleListen);
