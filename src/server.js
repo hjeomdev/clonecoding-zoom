cfc5c68cc525258a9c7b6bd9b2f26e8a7353bb58
Hyejin Eom <yazbyz@gmail.com>
2021-11-10 23:39:17 +0900

    [UPDATE] Express 세팅

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// view 세팅
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+// render 세팅
+app.get("/", (req, res) => res.render("home"));
 
+const handleListen = () => console.log('Listening on http://localhost:3000');
 app.listen(3000);
